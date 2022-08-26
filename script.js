00b923759e394d226ddaa57de88e67d420780e95
unknown <nhatlinh0264>
2022-08-26 10:29:13 +0700

    add-26-8

--- script.js
+++ script.js
@@ -1,75 +1,140 @@
-const listNav = []
-function html([first,...strings], ...values) {
+const listNav = {
+    arr: [],
+    tog: true
+  }
+  const $ = document.querySelector.bind(document)
+  const $$ = document.querySelectorAll.bind(document)
+  
+  
+  function html([first, ...strings], ...values) {
     return values.reduce(
-        (acc,cur) => acc.concat(cur,strings.shift()),
-        [first]
-        )
-    .join('')
-}
-
-function createStore(reducer) {
+        (acc, cur) => acc.concat(cur, strings.shift()),
+           [first]
+      )
+      .filter(x => x && x !== true || x === 0)
+      .join('')
+  }
+  
+  function createStore(reducer) {
     let state = reducer()
     const rootElement = new Map()
-    
-
+  
+  
     function render() {
-        for (const [root, component] of rootElement) {
-            const output = component()
-            root.innerHTML = output
-        }
+      for (const [root, component] of rootElement) {
+        const output = component()
+        root.innerHTML = output
+        console.log(rootElement);
+      }
     }
-
+  
     return {
-        attach(component, root) {
-            rootElement.set( root, component)
-            render()
-        },
-        connect(selector = state => state) {
-            return component => (props, ...args) =>
-            component(Object.assign({}, props,selector(state), ...args))
-        },
-        dispatch(action,...args){
-            state = reducer(state,action,args)
-            render()
-        }
+      attach(component, root) {
+        rootElement.set(root, component)
+        render()
+      },
+      connect(selector = state => state) {
+        return component => (props, ...args) =>
+          component(Object.assign({}, props, selector(state), ...args))
+      },
+      dispatch(action, ...args) {
+        state = reducer(state, action, args)
+        console.log(listNav);
+        render()
+      }
     }
-}
-
-function reducer(state,action,args) {
-    
+  }
+  
+  function reducer(state = {listNav}, action, args) {
+  
     switch (action) {
-        case 'create':
-            const a = document.querySelector('input[type="text"]')
-            listNav.unshift(a.value) 
-            console.log(listNav, 'add' ,args)
-            
-            break
-        case 'delete':
-            listNav.splice(listNav.findIndex( a => a==args),1)
-            break
-        default:
-            return state
-            
+      case 'create':
+        const a = $('input[type="text"]')
+        console.log(a.value, state);
+        if (a.value != '') {
+          listNav.arr.push({name:a.value,update: false})
+        }
+        break
+       /* return a != '' && {
+          ...state,
+          arr: [...state.arr, ab]
+        }*/
+      case 'delete':
+        if (confirm('are you sure') == true)
+          listNav.arr.splice(args, 1)
+  
+        break
+      case 'enter':
+        if (event.key === 'Enter')
+          dispatch('create')
+        break
+      case 'toggle':
+        listNav.tog = !listNav.tog
+        break
+      case 'update':
+        listNav.arr[args].update = !listNav.arr.update
+        break
+      case 'change':
+        if($('#change').value != '')
+        {listNav.arr[args].name = $('#change').value
+        console.log(listNav.arr[args].name);
+        listNav.arr[args].update = !listNav.arr[args].update}
+        break
+      default:
+        return state
+  
     }
-}
-
-
-
-function navElement(){
+  }
+  
+  
+  
+  
+  function inputBar() {
+    return `<input type="text" onkeypress="dispatch('enter')">
+           <button onClick="dispatch('create')">ADD</button>
+           <button onClick="dispatch('toggle')">Edit</button>
+      <p>List to do</p> `
+  }
+  
+  function buttonEdit(navNode,i) {
     return html`
-    <input type="text">
-    <button onClick="dispatch('create')">ADD</button>
-    <p>List to do</p>
-    <ul>
-        ${listNav.map(navNode =>
-            `<li>
-            <span>${navNode}</span>
-            <button onClick= "dispatch('delete','${navNode}')">x</button>
-            `)}
-    </ul>
+                    <span>
+                              <button onClick= "dispatch('delete','${i}')">X</button>
+                              <button onClick= "dispatch('update','${i}')">/</button>
+                    </span>`
+  }
+  const { attach, connect, dispatch } = createStore(reducer)
+  window.dispatch = dispatch()
+  const connector = connect()
+  
+  rootElements = $('#root')
+  connector(navElement)
+  console.log(connector(navElement));
+  attach(navElement, rootElements)
+  
+  function navElement() {
+    return inputBar()+ html`<ul>${listNav.arr.map((navNode,index)=>html`<li> <div class ="lis">${s(navNode, index)}</div></li>`)}</ul>`
+  }
+  
+  
+  function listNode(navNode, index) {
+    return html`<span>${navNode.name}</span>${listNav.tog && buttonEdit(navNode,index)}</div>`
+  }
+  
+  function updateList(navNode,index) {
+    return html`
+    <input id ="change" type="text" value="${navNode.name}" ><button onclick="dispatch('change','${index}')">Change</button>
     `
-}
-const { attach, connect, dispatch } = createStore(reducer)
-window.dispatch = dispatch()
-rootElement = document.querySelector('#root')
-attach(navElement, rootElement)
+  }
+  
+  function s(navNode,index) {
+     if(navNode.update == false)
+           return listNode(navNode,index)
+            else
+           return updateList(navNode,index)
+            
+  }
+  
+  
+  
+  
